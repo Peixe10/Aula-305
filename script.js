cf7019777fa2aca1c57bd8847da60261e3662db6
Jhonatan da costa <jhonatandacosta0509@gmail.com>
2018-07-20 08:44:38 -0300

    Menu realizado

---
+++ script.js
@@ -0,0 +1,16 @@
+//Primeiro exemplo
+
+/*var palpite;
+var palpite_certo=5;
+
+do {
+    palpite=parseInt(prompt("Adivinhe um numero de 1 a 10"));
+    if(palpite != palpite_certo){
+        alert("Errou!")
+    }
+    
+}
+while(palpite != palpite_certo);
+alert("Palpite correto")
+*/
+
