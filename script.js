3687bdb56cf81ee097043979a5fd5a41691eea36
Peixe <thefish0987@hotmail.com>
2018-07-20 11:53:53 -0300

    Final da aula

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ while(palpite != palpite_certo);
 alert("Palpite correto")
 */
 
+
+//EXERCICIOS DE FUNCTION
+
+/*
+1 - Criar um código que solicite a idade do usuário e retorne através de uma função a idade que o usuário terá na próxima copa do mundo.
+
+
+2 - Criar uma função que verifique se o ano é bissexto.
+
+
+3 - Criar uma função que receba um número e imprima de "numeroRecebido até 100". O número deverá ser recebido através do prompt e deverá utilizar a estrutura de repetição FOR.*/
+
+function ex1(){
+    
+idade=parseFloat(prompt("Qual é a sua idade"));
+
+alert("Você terá: " + (idade + 40) + " anos na copa de 2058" );
+
+}
+
+
+function ex2(){
+
+    var comida = {
+        "principal":["strogonoff","lasanha","pescada"],
+        "sobremesa":["petit gateau", "brigadeiro", "pudim","gelatina"],
+        "bebida":["água", "suco de laranja", "coca-cola"]
+    }
+
+    
+}
+
